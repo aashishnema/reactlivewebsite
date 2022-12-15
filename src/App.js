4d1abeacf6135f5a2e53f4ce3bb1a35c735787e5
aashishnema <aashish.nema4@gmail.com>
2022-12-15 14:01:50 +0530

    myReactPortfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+   <div>
+     <HelloWorld></HelloWorld>
+   </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HelloWorld(){
+    return (
+        <h1>Hello Ashish Kumar Nema</h1>
+    )
+}
+
+export default HelloWorld
