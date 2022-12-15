9054c6da3f97893d8d45f0ef93dc6e13095c4a05
aashishnema <aashish.nema4@gmail.com>
2022-12-15 17:55:54 +0530

    New Commit

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function HelloWorld(){
     return (
+
+        <>
         <h3>Hi my name is Ashish Kumar Nema, I work for Cognizant, Pune location </h3>
+        <p>1500*10 15000
+            170*100 17000
+            1800*5 9000
+            2500*2 5000
+            410*50 20500
+            2000*5 10000
+            3180*2  6360
+            1500*10   15000
+            230*50  11500</p>
+            </>
     )
 }
 
