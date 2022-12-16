8cf38107ee457253c466572828342cca1d007903
aashishnema <aashish.nema4@gmail.com>
2022-12-16 19:33:47 +0530

    adding dummy content

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,12 +1,74 @@
 import React from "react";
 
-function HelloWorld(){
+function HelloWorld() {
     return (
 
         <span className="bodyClass">
-        <h3>Hi my name is Ashish Kumar Nema, I work for Cognizant, Pune location. Welcome to my Portfolio. </h3>
-        
-            </span>
+            <h3>Hi my name is Ashish Kumar Nema, I work for Cognizant, Pune location. Welcome to my Portfolio. </h3>
+         <p>Education Guidelines (Typical):
+Science / Engineering graduate
+
+Purpose of the Role:
+Design develop and implement tasks/components of a work package / project as per client requirements; create a zero defect code and modify complex systems or develop new sub systems; develop comprehensive unit testing cases and plans; update knowledge assets and guide team to do the same; integrate work of developers WITH THE OBJECTIVE OF delivering high quality project deliverables WITHIN THE LIMITS OF delivery timelines security quality and performance guidelines.
+
+Areas of Responsibilities:
+" Requirements Gathering:
+' Participate in requirement gathering JAD (Joint Application Development) sessions and support documentation where applicable (business functional Technical UI usability data compliance data migration NFR 3rd party tools & products)
+Raise queries and get query resolution on areas which are unclear ambiguous and which need more information to the Technology Lead.
+
+Project Estimation:
+' Understand how the estimation (effort size etc.) is done at task level in the area of work concerned measure and report the actual effort against the estimated effort and ensure task progress reporting in C2.0 or any other applicable tool.
+
+Capability Development:
+Become an expert in the core skill and develop additional skills.
+
+Design & Analysis:
+' Create the HL Design and detailed design document with the support of architects for the assigned scope;
+Review all deliverables created by the Developer;
+Study non functional requirements and design for NFR as required;
+Understand the interdependencies of the various components in work packages being developed;
+Create detailed design and assign to developer; provide input to the Project Manager / Technical Lead to create the project plan from a technical perspective; support development of proof of concept as designed by Designer / Solution Architect; initiate development environment setup;
+Provide demo to customer for early feedback; create quality check points for all deliverables
+
+Coding:
+' Develop tier coding business layer coding interface development service development creation of stored procedures etc. as applicable for the work package / project;
+CTS and customer specific coding standards guidelines and best practices;
+Understand the database concepts and write efficient queries; raise clarifications / issues / concerns regarding work output to the lead;
+Seek review from peer / technical lead periodically; participate in setup activities for required environment setup; deploy code build in testing environment; conduct peer review during design coding and testing;
+Complete documentation as per the scope; prepare guidelines and checklist for development and testing activities;
+Provide feedback to design team; share feedback with developers to check if code is as per requirements;
+Incorporate changes as per feedback; provide necessary and timely status updates to the Supervisor / Technical Lead;
+Highlight any potential risks to the leads;
+Update traceability matrix for the work package developed; mentor the Developers to help them solve technical difficulties in the course of coding;
+Follow the SCM policies set for project; raise clarifications / issues / concerns regarding work output to the lead on time; provide necessary and timely status updates to the Supervisor / Technical Lead;
+Proactively highlight any potential risks to the lead; provide support on process audit activities; adhere to process and tools (usage of cognizant 2.0 awareness of Quality Management System).
+
+Testing:
+' Develop comprehensive unit testing cases and unit test plans; conduct comprehensive unit testing; create unit test logs;
+Prepare and support in preparation of unit test strategy; plan document and data requirements; automate unit testing and contribute to automation of unit testing;
+Conduct peer review of unit test cases; perform developer integration testing by integrating the components;
+Review test cases as prepared by the Developer.
+
+Defect Management:
+Participate in defect triage meetings; conduct RCA for defects identified;
+Rework on the code based on review comments from code review / defects raised in unit testing peer testing SIT UAT testing or any other relevant testing phases.
+Assist in transition of knowledge and to support organization with any defects raised post production as required
+
+
+Process Improvements and Adherence:
+Participate in innovation sessions; implement process improvements to reduce manual intervention (e.g. automation); support on process audit activities;
+Generate ideas to provide value add and share the same with the Technical Lead;
+Adhere to process and tools (usage of cognizant 2.0 awareness of Quality Management System IDE etc.).
+
+Knowledge Management:
+Contribute towards updating knowledge assets user manual online help document installation manual / scripts;
+Conduct knowledge transfer sessions for new joiners in the project team; conduct training through academy (if approved);
+Guide and mentor team junior members from a technical perspective to ensure quality delivery of the components; contribute / search / reuse all types of assets from repository.
+
+People management:
+Engage with team; resolve / conflict management within team; conduct internal trainings for developers within team. "</p>
+        </span>
+       
     )
 }
 
